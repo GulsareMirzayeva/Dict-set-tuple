82dc691bb0119da351347a8b1b826f18a000ca3a
GulsareMirzayeva <mirzayevagulsare@gmail.com>
2023-10-30 23:24:13 +0400

    first commit

---
+++ task2.py
@@ -0,0 +1,51 @@
+# Task1
+# cari_tuple = (1, 2, 3)
+
+# list = list(cari_tuple)
+
+# list.append(4)
+# list.append(5)
+
+# yeni_tuple = tuple(list)
+
+# print(yeni_tuple)
+
+
+
+
+# Task2
+# test_dict = {
+#     "ad": "Gulsare",
+#     "soyad": "Mirzayeva",
+#     "seher": "Karabakh"
+#     "yas": 23
+# }
+
+# secim = input("Daxil edin 1 (Keys) və ya 2 (Values): ")
+
+# if secim == "1":
+#     for key in test_dict.keys():
+#         print(key)
+# elif secim == "2":
+#     for value in test_dict.values():
+#         print(value)
+# elif not secim:
+#     print("Error,abi! Melumat daxil etmemisen")
+# else:
+#     print("Düzgün seçim edemmirsen de")
+
+
+# Task3
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6, 7}
+
+# ortaq_melumatlar = set()
+
+# for item in set1:
+#     if item in set2:
+#         ortaq_melumatlar.add(item)
+
+# if ortaq_melumatlar:
+#     print("Ortak məlumatlar:", ortaq_melumatlar)
+# else:
+#     print("Ortak məlumat tapammadım.")
